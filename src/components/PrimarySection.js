4dcabfde88aff6e2b7e631c2fd44233d9d55a73a
Molina60217N <molinasalas16@gmail.com>
2023-05-25 23:12:27 -0600

    primer intento de fixes de la primary section

--- src/components/PrimarySection.js
+++ src/components/PrimarySection.js
@@ -12,24 +12,29 @@ const PrimarySection = (props) => {
             <div className='main-container'>
                 <div className='text-container'>
                     <p className='small-title'>{props.smalltitle}</p>
-                        <div dangerouslySetInnerHTML={maintitle} className='main-title'/>
+                    <div dangerouslySetInnerHTML={maintitle} className='main-title' />
                     <p className='subtitle'>{props.subtitle}</p>
                     <div className='links-container'>
-                    <Button
-                        route='/ContactPage'
-                        text={props.giveacall} />
-                    <Link to='/' className='learn-more'>
-                        {props.learnmore + " >"}  
-                    </Link>
+                        <div className='give-call'>
+                            <Button
+                                route='contact-page'
+                                text={props.giveacall} />
+                        </div>
+                        {/* <Link to='/' className='learn-more'>
+                        {props.learnmore}<svg className='learn-more-arrow' width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1.17084 11.3083C1.025 11.1624 0.952087 10.9898 0.952087 10.7903C0.952087 10.5911 1.025 10.4187 1.17084 10.2728L5.44375 5.99992L1.15625 1.71242C1.02014 1.57631 0.952087 1.40617 0.952087 1.202C0.952087 0.997835 1.025 0.822835 1.17084 0.677002C1.31667 0.531169 1.48934 0.458252 1.68884 0.458252C1.88795 0.458252 2.06042 0.531169 2.20625 0.677002L7.10625 5.59159C7.16459 5.64992 7.206 5.71311 7.2305 5.78117C7.25462 5.84922 7.26667 5.92214 7.26667 5.99992C7.26667 6.0777 7.25462 6.15061 7.2305 6.21867C7.206 6.28672 7.16459 6.34992 7.10625 6.40825L2.19167 11.3228C2.05556 11.4589 1.88795 11.527 1.68884 11.527C1.48934 11.527 1.31667 11.4541 1.17084 11.3083Z" fill="#339999"/>
+</svg>
+                    </Link> */}
                     </div>
-                        <Newsletter/>
+                    <Newsletter />
                 </div>
                 <div className='image-section'>
                     <GatsbyImage
-                        image = {getImage(props.image.localFile)}
-                        alt = 'Home'
+                        image={getImage(props.image.localFile)}
+                        alt='Home'
                         className='home-image'
                     />
+                    <div className='orange-gradiant'></div>
                 </div>
             </div>
         </Wrapper>
@@ -43,35 +48,42 @@ const Wrapper = styled.div`
 .main-container{
     display: grid; 
     grid-template-columns: 1fr 1fr;
-    margin: 40px auto;
+    margin: 100px auto 40px 150px;
 }
 .text-container{
     display: flex;
     flex-direction: column;
     flex: start;
-    margin: 20px auto;
+    margin: 20px auto 20px auto;
+    width: 100%;
 }
 .small-title{
     font-size: 17px;
     line-height: 16px;
     word-spacing: 4px;
     letter-spacing: 2px;
-}
-.main-title p{
-    font-size: 60px;
     font-weight: 700;
 }
+.main-title p{
+    font-size: 70px;
+    font-weight: 700;
+    letter-spacing: -0.04em;
+    line-height: 70px;
+}
 .main-title span{
-    background: linear-gradient(89.63deg, rgb(51, 153, 153, 1) 5.4%, rgba(255, 153, 51) 100.53%);
+    background: linear-gradient(89.63deg, #339999 5.4%, #FF9933 49.53%);;
     /* background: linear-gradient(rgba(51, 153, 153, 0.9) 31.87%, rgba(255, 153, 51, 0.702) 48.3%); */
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     background-clip: text;
-    -webkit-text-fill-color:transparent;
 }
 .subtitle{
-    width: 50%;
+    font-weight: 600;
+    font-size: 16px;
+    color: rgba(0, 11, 40, 0.6);
+    width: 40%;
     line-height: 24px;
+    margin-top: 25px;
 }
 .links-container{
     display: flex;
@@ -80,9 +92,15 @@ const Wrapper = styled.div`
     margin-top: 40px;
     align-items: center;
 }
+.give-call{
+    margin-top: 15px;
+}
 .learn-more{
     color:#339999;
     text-decoration: none;
+}
+.learn-more-arrow{
+    margin: 0px 0px 2px 12px;
 }
 .image-section{
     background-color: #FF9933;
@@ -91,19 +109,35 @@ const Wrapper = styled.div`
     position: relative;
     overflow: visible;
     padding-bottom: 100px;
-    margin: 40px 0px 0px auto;
-    border-radius: 198.5px 0px 0px 198.5px;
-    width: 600px;
-    height: 400px;
+    margin: 20px 0px 0px auto;
+    border-radius: 259px 0px 0px 259px;
+    width: 800px;
+    height: 517px;
 }
+/* .image-section::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 50px;
+    background: radial-gradient(50% 50% at 50% 50%, rgba(255, 153, 51, 0.35) 0%, rgba(255, 204, 153, 0) 100%);
+} */
 .home-image{
     position: absolute;
-    width: 600px;
-    height: 400px;
-    border-radius: 198.5px 0px 0px 198.5px;
+    width: 800px;
+    height: 517px;
+    border-radius: 259px 0px 0px 259px;
     top: 0;
     right: 0;
-    transform: translate(0%, -15%);
+    transform: translate(0%, -11%);
+}
+.orange-gradiant{
+    position: absolute;
+    width: 150%;
+    height: 120%;
+    top: 230px;
+    background: radial-gradient(50% 50% at right, rgba(255, 153, 51, 0.35) 0%, rgba(255, 204, 153, 0) 100%);
 }
 
 @media only screen and (max-width: 1600px){
