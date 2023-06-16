df351132960850b27b499c2236873d01b141b896
AnthonnyE.R <76509736+ThonnyCR@users.noreply.github.com>
2023-06-15 21:44:28 -0600

    fix de src indefinido. debido a que la variable de entorno aun no esta disponible al renderizar por primera ves

--- src/utils/replaceHtml.js
+++ src/utils/replaceHtml.js
@@ -1,11 +1,15 @@
-const drupalBaseUrl = process.env.DRUPAL_BASE_URL;
-
+// I tried to use an environment variable (the one that already exists in the .env)
+// but it does not work because when it renders for the first time it is not defined,
+// so you have to refresh the page so that it is defined
 const replaceHtml = (html) => {
   const updatedHTML = html
     .replace(/<img[^>]*class="[^"]*"/g, "<img")
     .replace(/<img/g, '<img class="drupal-img"')
     .replace(/class="/g, 'class="')
-    .replace(/src="\/sites/g, `src="${drupalBaseUrl}/sites`);
+    .replace(
+      /src="\/sites/g,
+      'src="https://dev-decimo-drupal-site.pantheonsite.io/sites'
+    );
 
   return updatedHTML;
 };
